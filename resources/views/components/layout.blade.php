9d8866f516dc720a298ab77f1278119fda2634a6
Alia <xaertdinova.alia@gmail.com>
2024-11-16 10:45:07 +0300

    start

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>CatsClinik</title>
 </head>
 
-<body>
+<>
     <nav> 
         <a href="{{route('app')}}">Главное</a>
         <a href="{{route('services')}}">Услуги</a>
@@ -15,6 +16,8 @@
         <a href="{{route('admin')}}">Админ</a>
     </nav>
     {{$slot}}
+    <footer>
+    </footer>
 </body>
 
 </html>
