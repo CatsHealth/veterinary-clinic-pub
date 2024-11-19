44f752ddf6a89f107b6a096cbfc32dcbae326946
S0Artem <so.artem998@gmail.com>
2024-11-19 04:32:53 +0300

    сильно не ругайте пожалуйста

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
-<x-admin></x-admin>
+<x-admin>
 
+</x-admin>
 </x-layout>
--- resources/views/admin/service.blade.php
+++ resources/views/admin/service.blade.php
@@ -1,78 +1,96 @@
 <x-layout>
-<x-admin></x-admin>
-<form action="{{ route('service.store') }}" method="POST">
-    @csrf
+    <x-admin>
+        <form class="service-form" action="{{ route('service.store') }}" method="POST">
+            @csrf
 
-    <label for="name">Название:</label>
-    <input type="text" id="name" name="name" required maxlength="100" value="{{ old('name') }}">
-    @error('name')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
+            <div class="form-group service-name">
+                <label for="name">Название:</label>
+                <input type="text" id="name" name="name" class="service-name-input" required maxlength="100" value="{{ old('name') }}" placeholder="Введите название услуги">
+                @error('name')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    <label for="doctor">Доктор:</label>
-    <select id="doctor" name="doctor_1">
-        <option value="">Выберите врача</option>
-        @foreach($doctors as $doctor)
-            <option value="{{ $doctor->id }}" @if(old('doctor_1') == $doctor['id']) selected @endif>{{ $doctor->name }}</option>
-        @endforeach
-    </select>
+            <div class="form-group service-doctor">
+                <label for="doctor_1">Доктор 1:</label>
+                <select id="doctor_1" name="doctor_1" class="service-doctor-select">
+                    <option value="">Выберите врача</option>
+                    @foreach($doctors as $doctor)
+                        <option value="{{ $doctor->id }}" @if(old('doctor_1') == $doctor->id) selected @endif>{{ $doctor->name }}</option>
+                    @endforeach
+                </select>
+                @error('doctor_1')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    @error('doctor_1')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
+            <div class="form-group service-doctor">
+                <label for="doctor_2">Доктор 2:</label>
+                <select id="doctor_2" name="doctor_2" class="service-doctor-select">
+                    <option value="">Выберите врача</option>
+                    @foreach($doctors as $doctor)
+                        <option value="{{ $doctor->id }}" @if(old('doctor_2') == $doctor->id) selected @endif>{{ $doctor->name }}</option>
+                    @endforeach
+                </select>
+                @error('doctor_2')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    <select id="doctor" name="doctor_2">
-        <option value="">Выберите врача</option>
-        @foreach($doctors as $doctor)
-            <option value="{{ $doctor->id }}" @if(old('doctor_2') == $doctor['id']) selected @endif>{{ $doctor->name }}</option>
-        @endforeach
-    </select>
+            <div class="form-group service-doctor">
+                <label for="doctor_3">Доктор 3:</label>
+                <select id="doctor_3" name="doctor_3" class="service-doctor-select">
+                    <option value="">Выберите врача</option>
+                    @foreach($doctors as $doctor)
+                        <option value="{{ $doctor->id }}" @if(old('doctor_3') == $doctor->id) selected @endif>{{ $doctor->name }}</option>
+                    @endforeach
+                </select>
+                @error('doctor_3')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    @error('doctor_2')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
+            <div class="form-group service-price">
+                <label for="price">Цена:</label>
+                <input type="number" id="price" name="price" class="service-price-input" required value="{{ old('price') }}" placeholder="Введите цену услуги">
+                @error('price')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    <select id="doctor" name="doctor_3">
-        <option value="">Выберите врача</option>
-        @foreach($doctors as $doctor)
-            <option value="{{ $doctor->id }}" @if(old('doctor_3') == $doctor['id']) selected @endif>{{ $doctor->name }}</option>
-        @endforeach
-    </select>
+            <div class="form-group service-duration">
+                <label for="duration">Продолжительность:</label>
+                <input type="number" id="duration" name="duration" class="service-duration-input" required value="{{ old('duration') }}" placeholder="Введите продолжительность услуги">
+                @error('duration')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    @error('doctor_3')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
+            <div class="form-group service-caption">
+                <label for="caption">Заголовок:</label>
+                <input type="text" id="caption" name="caption" class="service-caption-input" required maxlength="255" value="{{ old('caption') }}" placeholder="Введите заголовок">
+                @error('caption')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    <label for="price">Цена:</label>
-    <input type="number" id="price" name="price" required value="{{ old('price') }}">
-    @error('price')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
+            <div class="form-group service-recommendation">
+                <label for="recommendation">Рекомендация:</label>
+                <textarea id="recommendation" name="recommendation" class="service-recommendation-textarea" placeholder="Введите рекомендации">{{ old('recommendation') }}</textarea>
+                @error('recommendation')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    <label for="duration">Продолжительность:</label>
-    <input type="number" id="duration" name="duration" required value="{{ old('duration') }}">
-    @error('duration')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
+            <div class="form-group service-description">
+                <label for="description">Описание:</label>
+                <textarea id="description" name="description" class="service-description-textarea" placeholder="Введите описание услуги">{{ old('description') }}</textarea>
+                @error('description')
+                    <div class="error-message">{{ $message }}</div>
+                @enderror
+            </div>
 
-    <label for="caption">Заголовок:</label>
-    <input type="text" id="caption" name="caption" required maxlength="255" value="{{ old('caption') }}">
-    @error('caption')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
-
-    <label for="recommendation">Рекомендация:</label>
-    <textarea type="text" id="recommendation" name="recommendation" >{{ old('recommendation') }}</textarea>
-    @error('recommendation')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
-
-    <label for="description">Описание:</label>
-    <textarea id="description" name="description">{{ old('description') }}</textarea>
-    @error('description')
-        <div style="color: red;">{{ $message }}</div>
-    @enderror
-    
-    <button type="submit">Сохранить</button>
-</form>
+            <button type="submit" class="service-submit-btn">Сохранить</button>
+        </form>
+    </x-admin>
 </x-layout>
