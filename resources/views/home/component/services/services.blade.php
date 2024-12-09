4405c88a1a2d7a6c2f0923c05a74c7cc879748a2
Alia <xaertdinova.alia@gmail.com>
2024-12-09 20:25:24 +0300

    Adding a block and site style

--- resources/views/home/component/services/services.blade.php
+++ resources/views/home/component/services/services.blade.php
@@ -7,6 +7,9 @@
                     <h2>Услуги</h2>
                     <p>Общие осмотры и профилактика: Регулярные осмотры помогут выявить проблемы на ранних стадиях.</p>
                 </div>
+                <div class="services-image">
+                    <img src="{{asset('/img/paws-services.png')}}">
+                </div>
                 <div class="services-list">
 
                     @foreach ($services as $service)  
