5b30734d88fe208e75c077ad85687db3d9119320
Alia <xaertdinova.alia@gmail.com>
2024-12-12 08:23:46 +0300

    mobile

--- resources/views/home/component/staff/staff.blade.php
+++ resources/views/home/component/staff/staff.blade.php
@@ -3,19 +3,47 @@
     <div class="clinic">
         <div class="container">
             <h2>Наша клиника</h2>
-            <div class="clinic-content-wrapper">  
-                <div class="clinic-photos">
-                    <div id="main-image-container">
-                        <img src="{{asset('/img/clinick1.png')}}" class="main-image-img">
-                    </div>
-
-                    <div class="clinic-thumbnails">
-                        <img src="{{asset('/img/clinick2.png')}}" class="clinic-photo" data-full-src="{{asset('/img/clinick2.png')}}">
-                        <img src="{{asset('/img/clinick3.png')}}" class="clinic-photo" data-full-src="{{asset('/img/clinick3.png')}}">
-                    </div>
-                </div>
-                <img src="{{asset('/img/paws-clinic.png')}}" class="paws-background">  
+            <div class="clinic-photos">
+              <div class="gallery__photo-full">
+                <img class="main-image-img" src="{{asset('/img/clinick1.png')}}" alt="Большое фото">
             </div>
+            <div class="gallery__photo-previews">
+            <img src="{{asset('/img/clinick1-1.png')}}" class="clinic-photo">
+                <img src="{{asset('/img/clinick2.png')}}" class="clinic-photo">
+                <img src="{{asset('/img/clinick3.png')}}" class="clinic-photo">
+            </div>  
+            </div>
+            
+            <img src="{{asset('/img/paws-clinic.png')}}" class="paws-background">
         </div>
     </div>
+    </div>
 </section>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        var photos = [
+            '{{asset("/img/clinick1.png")}}',
+            '{{asset("/img/clinick2-2.png")}}',
+            '{{asset("/img/clinick3-3.png")}}'
+        ];
+
+        var thumbnails = document.querySelectorAll('.clinic-photo');
+        var fullPhoto = document.querySelector('.main-image-img');
+
+        if (photos.length !== thumbnails.length) {
+            console.error("Количество больших и маленьких изображений не совпадает!");
+            return;
+        }
+
+        const addThumbnailClickHandler = (index) => {
+            thumbnails[index].addEventListener('click', () => {
+                fullPhoto.src = photos[index];
+            });
+        };
+
+        for (let i = 0; i < thumbnails.length; i++) {
+            addThumbnailClickHandler(i);
+        }
+    });
+</script>
