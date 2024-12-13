515b38e9a5810ca63449a382286fe807cdaa8aa8
mart-cat <as9060405@gmail.com>
2024-12-13 11:41:18 +0300

    lll

--- resources/views/doctor/index.blade.php
+++ resources/views/doctor/index.blade.php
@@ -1,24 +1,24 @@
 @vite(['resources/views/doctor/doctor.css'])
 <x-layout>
     <div class="container">
-    <form method="GET" action="{{ url()->current() }}">
-        <div class="form-container">
-            <div class="form-group">
-                <label for="serviceSelect">Выберите услугу:</label>
-                <select id="serviceSelect" name="service_id" class="form-control" onchange="this.form.submit()">
-                    <option value="">Все услуги</option>
-                    @foreach($services as $service)
-                        <option value="{{ $service->id }}" {{ $selectedServiceId == $service->id ? 'selected' : '' }}>
-                            {{ $service->name }}
-                        </option>
-                    @endforeach
-                </select>
+        <form method="GET" action="{{ url()->current() }}">
+            <div class="form-container">
+                <div class="form-group">
+                    <label for="serviceSelect">Выберите услугу:</label>
+                    <select id="serviceSelect" name="service_id" class="form-control" onchange="this.form.submit()">
+                        <option value="">Все услуги</option>
+                        @foreach($services as $service)
+                            <option value="{{ $service->id }}" {{ $selectedServiceId == $service->id ? 'selected' : '' }}>
+                                {{ $service->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                @include('doctor.show')
+
             </div>
+    </div>
 
-            @include('doctor.show')
 
-            </div>
-           </div>
-
-    
 </x-layout>
